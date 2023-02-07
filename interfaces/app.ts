0a8ca0399943c123eec9be32bbdcabcaefcea25b
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-07 16:18:48 -0300

    fixed Readme

--- interfaces/app.ts
+++ interfaces/app.ts
@@ -1,14 +1,22 @@
 interface Pessoa {
-    nome: string,
-    idade: number,
-    cpf?: string,
-    profissao?: string
+  nome: string;
+  idade: number;
+  cpf?: string;
+  profissao?: string;
 }
-
 
 const pessoa: Pessoa = {
-    nome: "Jefferson",
-    idade: 23,
-    cpf: "179.437.907-07",
-    profissao: "Developer"
-}
+  nome: "Jefferson",
+  idade: 23,
+  cpf: "179.437.907-07",
+  profissao: "Developer",
+};
+
+const outraPessoa: Pessoa = {
+  nome: "Casale",
+  idade: 15,
+};
+
+const teste: Array<Pessoa> = [pessoa, outraPessoa];
+
+const nome = teste.map((item) => console.log(item.nome));
