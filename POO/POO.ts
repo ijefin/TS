a54de789aedebbc60939fa4de5deb048cc5416bd
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-08 18:35:53 -0300

    lets intro to POO

--- POO/POO.ts
+++ POO/POO.ts
@@ -1,12 +1,21 @@
 class User {
-  name: string = "Jefferson";
-  age: number = 23;
+  names: string;
+  age: number;
+
+  constructor(names: string, age: number) {
+    this.names = names;
+    this.age = age;
+  }
 
   showInfo = () => {
-    console.log(`Your name: ${this.name}\nYour age: ${this.age}`);
+    console.log(`Your name: ${this.names}\nYour age: ${this.age}`);
   };
 }
 
-const Jeff = new User();
+const Jeff = new User("jefferson", 23);
 
 Jeff.showInfo();
+
+const Anna = new User("Anna", 22);
+
+Anna.showInfo();
