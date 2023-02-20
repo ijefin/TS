e838ad38f4fc44cb98a6f35693813bdd23e533c7
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-20 17:38:05 -0300

    added new array tests

---
+++ interfaces/app3.ts
@@ -0,0 +1,15 @@
+const array: Array<number> = [1, 2, 3];
+
+const arrayString: string[] = [
+  "Jeff",
+  "Luca",
+  "Carlo",
+  "Jeff",
+  "Luca",
+  "Carlo",
+  "Jeff",
+  "Luca",
+  "Carlo",
+];
+
+console.log(arrayString.length)
