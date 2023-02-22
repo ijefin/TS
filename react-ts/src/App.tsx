56432b0b60b0478b1a6fa56bc99854b76d8ccd53
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-22 09:50:15 -0300

    react state

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,20 +1,37 @@
-import { ChakraProvider, Input, Box, Center } from "@chakra-ui/react";
+import { ChakraProvider, Button, Input } from "@chakra-ui/react";
 import { Login } from "./services/login/Login";
+import { useState } from "react";
 
 function App() {
+  const [valor, setValor] = useState(0);
+  const [inputValue, setinputValue] = useState("");
+
+  const aumentar = () => {
+    setValor(valor + 1);
+  };
+
+  const diminuir = () => {
+    setValor(valor - 1);
+  };
+
+  const getValue = (e: any) => {
+    setinputValue(e.target.value);
+  };
+
+  const showToast = () => {
+    alert(inputValue);
+  };
+
   return (
     <>
       <ChakraProvider>
-        <Box minHeight={`100vh`} backgroundColor={`#ee6521e2`}>
-          <Center>
-            <h1>Faça login para continuar!</h1>
-          </Center>
-
-          <Input size={"sm"} placeholder={`Seu email`} />
-          <Input size={"sm"} placeholder={`Sua senha`} />
-
-          <button onClick={Login}>Entrar</button>
-        </Box>
+        <Button onClick={aumentar}>Aumentar</Button>
+        <h1>{valor}</h1>
+        <Button onClick={diminuir}>Diminuir</Button>
+        <h1>{valor}</h1>
+        <Input value={inputValue} onChange={getValue} type={`text`}></Input>
+        <h1>{inputValue}</h1>
+        <Button onClick={showToast}>Show!</Button>
       </ChakraProvider>
     </>
   );
