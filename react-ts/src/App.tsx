88450252b8963abbb151c2a890f8986ace878cfd
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-21 22:39:17 -0300

    added chakra ui

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "./components/Layout";
 import styled from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const Box = styled.div`
   background-color: orange;
@@ -8,18 +9,20 @@ const Box = styled.div`
 function App() {
   return (
     <>
-      <Layout>
-        <div className="login-content">
-          <Box>
-            <h1>Faça login para continuar!</h1>
-          </Box>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="text" />
-          <label htmlFor="Senha">Senha</label>
-          <input id="password" type="password" />
-          <button>Entrar</button>
-        </div>
-      </Layout>
+      <ChakraProvider>
+        <Layout>
+          <div className="login-content">
+            <Box>
+              <h1>Faça login para continuar!</h1>
+            </Box>
+            <label htmlFor="email">Email</label>
+            <input id="email" type="text" />
+            <label htmlFor="Senha">Senha</label>
+            <input id="password" type="password" />
+            <button>Entrar</button>
+          </div>
+        </Layout>
+      </ChakraProvider>
     </>
   );
 }
--- react-ts/src/components/Header/Header.tsx
+++ react-ts/src/components/Header/Header.tsx
@@ -1,19 +1,10 @@
 import "./Header.css";
 import styled from "styled-components";
 
-const Logo = styled.div`
-  background-color: orange;
-  border-radius: 2px;
-  padding-left: 15px
-  
-`;
-
 export const Header = () => {
   return (
     <>
-      <Logo>
-        <h1 className="header">Dio Bank</h1>
-      </Logo>
+      <h1>Dio Bank</h1>
     </>
   );
 };
