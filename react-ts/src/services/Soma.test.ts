39832f8671835fcafd65d1d4c078fa7227ee3533
ijefin <jeffersonmecmiler@hotmail.com>
2023-02-22 01:48:49 -0300

    added TDD pratices

--- react-ts/src/services/Soma.test.ts
+++ react-ts/src/services/Soma.test.ts
@@ -1,7 +1,28 @@
-import { soma } from "./Soma";
+import { multiplica, soma } from "./Soma";
 describe("soma", () => {
   it("Should add 1 to given number", () => {
     const value = soma(5);
     expect(value).toBe(6);
+  });
+});
+
+describe("multiplica", () => {
+  it("Should multiply a given number for 2", () => {
+    const value = multiplica(5, 2);
+    expect(value).toBe(10);
+  });
+});
+
+describe("multiplica", () => {
+  it("Should multiply a given number for 3", () => {
+    const value = multiplica(5, 3);
+    expect(value).toBe(15);
+  });
+});
+
+describe("multiplica", () => {
+  it("should show an error that says the multiplier is invalid", () => {
+    const value = multiplica(5, 5);
+    expect(value).toBe("Multiplicador não aceito!");
   });
 });
--- react-ts/src/services/Soma.tsx
+++ react-ts/src/services/Soma.tsx
@@ -2,3 +2,10 @@ export const soma = (a: number) => {
   return a + 1;
 };
 
+export const multiplica = (valor: number, mult: number): number | string => {
+  if (mult === 2 || mult === 3) {
+    return valor * mult;
+  }
+
+  return `Multiplicador não aceito!`;
+};
